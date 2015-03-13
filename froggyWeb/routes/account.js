a1fe9aab35d061af2051b0ecf86a5c7ed8cef5f2
dsorbito <daxsorbito@gmail.com>
2015-03-13 19:46:13 +0800

    added registration implementation

--- froggyWeb/routes/account.js
+++ froggyWeb/routes/account.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var Membership = require('membership');
+
 
 router.route('/login')
     .post(passport.authenticate('local', {
@@ -12,8 +14,12 @@ router.route('/login')
 );
 
 router.route('/register')
-    .post(function(){
-
+    .post(function(request, response){
+        var mem = new Membership('membership');
+        mem.register(request.body.username, request.body.password, request.body.confirm, function(err, result){
+            console.log(result);
+            response.render('index', { title: 'Express' , user: result.user.email, err: request.flash('error')});
+        });
     });
 
 
